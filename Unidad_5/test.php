3cce3857649f397d1cb02a97ea3d90e0a8a0789c
osvalucero <oslu_20@alu.uabcs.mx>
2023-10-31 18:13:09 -0700

    larabel

---
+++ Unidad_5/test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <? php $name = "juanito" ?>
+    <? php $test = 123*2 ?>
+
+    <? hola $name $test ?>
+    
+</body>
+</html>

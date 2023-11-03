d6143ba2ac7ecc6c89e031ccda3506d214f7cb5e
osvalucero <oslu_20@alu.uabcs.mx>
2023-11-02 18:42:54 -0700

    rutas

---
+++ Unidad_5/my-app/routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/saludar/{name}/', function ($name) {
+    return view('saludo',['name' => $name]);
+})->where(['name' => '[A-Za-z]+']);
+
+Route::get('/saludo/{name}/{lastname?}', function ($name, $lastname=' ') {
+    return 'Hola '.$name.' '.$lastname;
+})->where(['name' => '[A-Za-z]+', 'lastname' => '[A-Za-z]+']);
+
+Route::get('/operaciones/{operacion}/{num1}/{num2}', function ($operacion,$num1, $num2) {
+    switch($operacion){
+        case "suma":
+            return $num1 + $num2;
+        break;
+        case "resta":
+            return $num1 - $num2;
+        break;
+        case "multi":
+            return $num1 * $num2;
+        break;
+        case "div":
+            return $num1 / $num2;
+        break;
+    }
+    
+})->where(['operacion' => '(suma|resta|multi|div)','num1' => '[0-9]+','num2' => '[0-9]+']);
+
+
